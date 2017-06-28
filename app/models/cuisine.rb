a531f4e7fbf1b248c83ae9b349d5c9976f41c477
Raghu Betina <raghu@firstdraft.com>
2017-06-28 02:28:29 +0000

    Direct association between cuisines and bookmarks

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,6 +1,9 @@
 class Cuisine < ApplicationRecord
   # Direct associations
 
+  has_many   :bookmarks,
+             :dependent => :destroy
+
   has_many   :dishes,
              :foreign_key => "cuisines_id",
              :dependent => :destroy
