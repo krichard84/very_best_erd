948f737c73ac59d3a1ae64f7b11fef27ddc4f4e0
Raghu Betina <raghu@firstdraft.com>
2017-06-28 02:28:29 +0000

    Direct association between users and dishes

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,8 @@
 class Dish < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   has_many   :bookmarks,
              :foreign_key => "dishes_id",
              :dependent => :destroy
